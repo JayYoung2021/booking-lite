77bbfdb60fc6c50628e6eb52f8b60b1c3e3bd4d9
Jay Young <1564772491young@gmail.com>
2022-12-23 00:06:18 +0800

    Complete tests for room_router

--- tests/test_routers_room.py
+++ tests/test_routers_room.py
@@ -117,6 +117,79 @@ def test_read_room_by_price():
            or (data[0]['price'] == 1000, data[1]['price'] == 998)
 
 
+def test_update_room_partially():
+    response = client.post(
+        '/rooms/',
+        json={
+            'room_number': '124',
+            'type_': 'twin',
+            'price': 126.66
+        }
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    id_ = data['id']
+
+    response = client.patch(
+        f'/rooms/{id_}',
+        json={
+            'price': 124.44
+        }
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['id'] == id_
+    assert data['price'] == 124.44
+
+
+def test_update_room_fully():
+    response = client.post(
+        '/rooms/',
+        json={
+            'room_number': '148',
+            'type_': 'single',
+            'price': 1500
+        }
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    id_ = data['id']
+
+    response = client.patch(
+        f'/rooms/{id_}',
+        json={
+            'room_number': '160',
+            'type_': 'family',
+            'price': 162.22
+        }
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['id'] == id_
+    assert data['room_number'] == '160'
+    assert data['type_'] == 'family'
+    assert data['price'] == 162.22
+
+
+def test_delete_room():
+    response = client.post(
+        '/rooms/',
+        json={
+            'room_number': '178',
+            'type_': 'twin',
+            'price': 18
+        }
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    id_ = data['id']
+
+    response = client.delete(
+        f'/rooms/{id_}/'
+    )
+    assert response.status_code == 204, response.text
+
+
 @pytest.fixture(scope='session', autouse=True)
 def db_conn():
     yield
