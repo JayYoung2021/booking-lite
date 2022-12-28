a0064c2c268c0db62ac075b12c2d8a101a99ab62
Jay Young <1564772491young@gmail.com>
2022-12-28 22:35:06 +0800

    Add CORS support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 import models
 from database import engine
@@ -14,6 +15,19 @@ from routers import (
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    # "http://localhost",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(admin_me_router)
 app.include_router(admin_router)
 app.include_router(user_router)
